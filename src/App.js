0380a57377d7c5fa5080881cb28c172e70b47444
Herlock <fidanur23@gmail.com>
2020-10-16 00:36:11 +0500

    delete test files and icons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
-//import TodoList from './Todo/TodoList'
 
 function App() {
-/*  let [todos, setTodos] = React.useState([
-    {id:1, completed: false, title: 'ts'},
-    {id:2, completed: false, title: 'react'},
-    {id:3, completed: false, title: 'git'}
-  ])
-  
-function toggleTodo(id) {
-  setTodos(
-    todos = todos.map(todo => {
-    if (todo.id === id) {
-      todo.completed = !todo.completed
-    }
-    return todo
-    })
-  )
-}
-*/
+
   return (
     <div className='wrapper'>
-      <div className='header'> Head </div>
+      <div className='header'>
+      head
+      </div>
       <nav className='navbar'> Nav </nav>
-      <div className='content' > <img src='https://i.imgur.com/ZSzKRBo.jpg' alt='img' className='icon'/>ку</div>
+      <div className='content'>
+      <div>asdasdfsadfsdagfdgsagsdfsadfsadfdsagdfgsdafsdagafsdfegsdafsdgdaf</div>
+      <div>asdasdfsadfsdagfdgsagsdfsadfsadfdsagdfgsdafsdagafsdfegsdafsdgdaf</div>
+      <div>sadasddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+      <div>4</div>
+      <div>5</div>
+      
+      
+      </div>
       <div className='footer'>footer</div>
     </div>
   );
