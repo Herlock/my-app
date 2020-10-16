c250c3f72b170fc207c73e3b609e4405244e3d46
Herlock <fidanur23@gmail.com>
2020-10-16 07:57:39 +0500

    svg иконки, гриды и флексы

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
+import owl from './img/main-icon.svg'
 
 function App() {
 
   return (
     <div className='wrapper'>
       <div className='header'>
-      head
+      <img src={owl} alt='owl'/>
+      <h1>Head</h1> 
       </div>
       <nav className='navbar'> Nav </nav>
       <div className='content'>
-      <div>asdasdfsadfsdagfdgsagsdfsadfsadfdsagdfgsdafsdagafsdfegsdafsdgdaf</div>
-      <div>asdasdfsadfsdagfdgsagsdfsadfsadfdsagdfgsdafsdagafsdfegsdafsdgdaf</div>
-      <div>sadasddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-      <div>4</div>
-      <div>5</div>
-      
-      
+        <div className='box-1'> <img src='https://i.imgur.com/ZSzKRBo.jpg' alt='profile' /> </div>
+        <div className='box-2'>asdasdfsadfsdagfdgsagsdfsadfsadfdsagdfgsdafsdagafsdfegsdafsdgdaf</div>
+        <div className='box-3'>sadasddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+        <div className='box-4'>4</div>
+        <div className='box-5'>5</div>
       </div>
-      <div className='footer'>footer</div>
+      <div className='footer'><img src={owl} alt='owl'/></div>
     </div>
   );
 }
